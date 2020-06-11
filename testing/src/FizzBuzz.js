b37c2640ef4746c887d809bc06439af80d16c794
Jan Redmann <janredmann1@gmail.com>
2020-06-11 11:58:23 +0200

    add testing

---
+++ testing/src/FizzBuzz.js
@@ -0,0 +1,16 @@
+
+
+
+// function fizzBuzz(num) {
+//     if (num % 15 === 0) return 'fizzbuzz';
+//     if (num % 3 === 0) return 'fizz';
+//     if (num % 5 === 0) return 'buzz'
+//     return num;
+// }
+function fizzBuzz(num) {
+    let string = '';
+    if (num % 3 === 0) string += 'fizz';
+    if (num % 5 === 0) string += 'buzz'
+    return string || num;
+}
+// const fizzBuzz = num => (num % 3 ? '' : 'fizz') + (num % 5 ? '' : 'buzz') || num
